07a987c941896a98859f7e5cca8691da108c7359
15chrjef <15chrjef@gmail.com>
2016-09-14 16:41:15 -0700

    Complete tree, start Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,30 +2,49 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.collection = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.collection[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.collection.hasOwnProperty(node);
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function(nodeToDelete) {
+  // for every edge belonging to nodeToDelete, remove nodeToDelete from the outbound edge
+  this.collection[nodeToDelete].forEach(function(otherNode) {
+
+  });
+  
+  delete this.collection[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.collection[fromNode].indexOf(toNode) >= 0;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.collection[fromNode].push(toNode);
+  this.collection[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  this.collection[fromNode] = this.collection[fromNode].filter(function(node) { 
+    return node !== toNode;
+  });
+
+  this.collection[toNode] = this.collection[toNode].filter(function(node) {
+    return node !== fromNode;
+  });
 };
 
 // Pass in a callback which will be executed on each node of the graph.
