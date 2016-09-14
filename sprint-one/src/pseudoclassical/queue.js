6d7c93ad5365e765c85fb1ce82f4df4ffd1ac867
15chrjef <15chrjef@gmail.com>
2016-09-14 16:19:59 -0700

    Complete linked list

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.top = 0;
+  this.bottom = 0;
 };
+
+Stack.prototype.enqueue = function() {
+
+};
+
+Stack.prototype.dequeue = function() {
+  
+};
+
+Stack.prototype.size = function() {
+  
+};
+
 
 
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (list.tail === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      var oldHead = list.head;
+      list.head = oldHead.next;
+      return oldHead.value;
+    }
   };
 
   list.contains = function(target) {
+    var node = list.head;
+    while (node !== null) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
