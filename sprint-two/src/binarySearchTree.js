a1fc7761eab65d838816b95c2600b9717b83d507
15chrjef <15chrjef@gmail.com>
2016-09-14 20:02:55 -0700

    Complete BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,45 @@
 var BinarySearchTree = function(value) {
+  this.left = null;
+  this.right = null;
+  this.value = value;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  if (this.value < value) {
+    if (this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else if (this.value > value) {
+    if (this.left === null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  } else if (this.value < value) {
+    return this.right ? this.right.contains(value) : false;
+  } else if (this.value > value) {
+    return this.left ? this.left.contains(value) : false;
+  }
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
